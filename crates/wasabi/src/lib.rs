5e40cfe7463ebd9c1e36e5dab617014afb15e0d2
Jakob Getz <jakob@getz.de>
2023-10-31 17:52:54 +0900

    first steps to compile wasabi to wasm

--- crates/wasabi/src/lib.rs
+++ crates/wasabi/src/lib.rs
@@ -1,5 +1,14 @@
+use wasm_bindgen::prelude::*;
+
 pub mod instrument;
 pub mod options;
 
 #[cfg(test)]
 mod tests;
+
+#[wasm_bindgen]
+pub fn instrument(bytes: &[u8], _options: String) -> Vec<u8> {
+    let mut module = wasabi_wasm::Module::from_bytes(bytes).unwrap().0;
+    instrument::add_hooks(&mut module, options::HookSet::new(), false);
+    module.to_bytes().unwrap()
+}
