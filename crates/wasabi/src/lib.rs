4ed115c45deb5fbe2824a7c2741e8532534e10d2
Doehyun Baek <doehyunbaek@gmail.com>
2023-11-02 16:15:56 +0900

    better error handling

--- crates/wasabi/src/lib.rs
+++ crates/wasabi/src/lib.rs
@@ -7,8 +7,22 @@ pub mod options;
 mod tests;
 
 #[wasm_bindgen]
-pub fn instrument(bytes: &[u8], _options: String) -> Vec<u8> {
-    let mut module = wasabi_wasm::Module::from_bytes(bytes).unwrap().0;
-    instrument::add_hooks(&mut module, options::HookSet::new(), false);
-    module.to_bytes().unwrap()
+pub fn instrument(bytes: &[u8]) -> Vec<u8> {
+    let module = wasabi_wasm::Module::from_bytes(bytes);
+    match module {
+        Ok((mut m, _, _)) => {
+            instrument::add_hooks(&mut m, options::HookSet::new(), false);
+            match m.to_bytes() {
+                Ok(r) => r,
+                Err(_) => {
+                    println!("PANIIIC 2");
+                    panic!()
+                }
+            }
+        }
+        Err(_) => {
+            println!("PANIIIC");
+            panic!()
+        }
+    }
 }
