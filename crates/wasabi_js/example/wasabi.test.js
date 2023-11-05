2ef0c7861ac97712b6df8ce2b1932c171812eb59
Doehyun Baek <doehyunbaek@gmail.com>
2023-11-05 22:14:50 +0900

    add wasabi_js crate

---
+++ crates/wasabi_js/example/wasabi.test.js
@@ -0,0 +1,14 @@
+import { expect, test } from 'vitest'
+
+const wasabi = require("../pkg");
+const fs = require('fs');
+
+test("add.wasm", async () => {
+    const buf = fs.readFileSync('./example/add.wasm');
+    const arr = new Uint8Array(buf);
+    const { instrumented, js } = wasabi.instrument_wasm({ original: arr });
+    const res = eval(js);
+    const buff = new Uint8Array(instrumented);
+    const { instance } = await WebAssembly.instantiate(buff, res);
+    expect(instance.exports.add(1, 2)).toBe(3);
+});
---
+++ crates/wasabi_js/src/lib.rs
@@ -0,0 +1,33 @@
+use serde::{Deserialize, Serialize};
+use wasabi::{instrument::add_hooks, options::HookSet};
+use wasabi_wasm::Module;
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
+
+#[derive(Serialize, Deserialize)]
+pub struct Input {
+    pub original: Vec<u8>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Output {
+    pub instrumented: Vec<u8>,
+    pub js: String,
+}
+
+#[wasm_bindgen]
+pub fn instrument_wasm(val: JsValue) -> Result<JsValue, JsValue> {
+    console_error_panic_hook::set_once();
+    let Input { original } = serde_wasm_bindgen::from_value(val)?;
+    // TODO: make hookset configurable
+    let _enabled_hooks = HookSet::all();
+    // TODO: make optnodejs configurable
+    let opt_nodejs = false;
+
+    let (mut module, _offsets, _warnings) = Module::from_bytes(&original).unwrap();
+    let (js, hook_count) = add_hooks(&mut module, _enabled_hooks, opt_nodejs).unwrap();
+    let instrumented = module.to_bytes().unwrap();
+    println!("inserted {hook_count} low-level hooks");
+
+    let output = Output { instrumented, js };
+    Ok(serde_wasm_bindgen::to_value(&output)?)
+}
