6269db20e606b27b935c0e09dafa5d26dee65aca
Doehyun Baek <doehyunbaek@gmail.com>
2023-11-06 10:00:16 +0900

    add hello.wasm demo

--- crates/wasabi_js/example/wasabi.test.js
+++ crates/wasabi_js/example/wasabi.test.js
@@ -12,3 +12,13 @@ test("add.wasm", async () => {
     const { instance } = await WebAssembly.instantiate(buff, res);
     expect(instance.exports.add(1, 2)).toBe(3);
 });
+
+test("hello.wasm", async () => {
+    const buf = fs.readFileSync('./example/hello.wasm');
+    const arr = new Uint8Array(buf);
+    const { instrumented, js } = wasabi.instrument_wasm({ original: arr });
+    const res = eval(js);
+    res.env = {print: console.log}
+    const buff = new Uint8Array(instrumented);
+    const { instance } = await WebAssembly.instantiate(buff, res);
+});
