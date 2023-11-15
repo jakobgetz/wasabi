31f24457752942af49456deabc5130064994ac10
Doehyun Baek <doehyunbaek@gmail.com>
2023-11-15 15:58:55 +0900

    get byte slice for instrument_wasm

--- crates/wasabi_js/src/lib.rs
+++ crates/wasabi_js/src/lib.rs
@@ -3,10 +3,6 @@ use wasabi::{instrument::add_hooks, options::{HookSet, Hook}};
 use wasabi_wasm::Module;
 use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
-#[derive(Serialize, Deserialize)]
-pub struct Input {
-    pub original: Vec<u8>,
-}
 
 #[derive(Serialize, Deserialize)]
 pub struct Output {
@@ -15,9 +11,8 @@ pub struct Output {
 }
 
 #[wasm_bindgen]
-pub fn instrument_wasm(val: JsValue) -> Result<JsValue, JsValue> {
+pub fn instrument_wasm(val: &[u8]) -> Result<JsValue, JsValue> {
     console_error_panic_hook::set_once();
-    let Input { original } = serde_wasm_bindgen::from_value(val)?;
     // TODO: make hookset configurable
     let mut _enabled_hooks = HookSet::new();
     for hook in [Hook::Begin, Hook::Call, Hook::Global, Hook::Load, Hook::Store, Hook::MemoryGrow, Hook::TableSet, Hook::TableGet, Hook::End] {
@@ -26,7 +21,7 @@ pub fn instrument_wasm(val: JsValue) -> Result<JsValue, JsValue> {
     // TODO: make optnodejs configurable
     let opt_nodejs = false;
 
-    let (mut module, _offsets, _warnings) = Module::from_bytes(&original).unwrap();
+    let (mut module, _offsets, _warnings) = Module::from_bytes(&val).unwrap();
     let (js, hook_count) = add_hooks(&mut module, _enabled_hooks, opt_nodejs).unwrap();
     let instrumented = module.to_bytes().unwrap();
     println!("inserted {hook_count} low-level hooks");
